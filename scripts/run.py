d3211263177d0436ea976a0d777d122cc3b82b8d
Sergio Oliveira <sergio@tracy.com.br>
2013-10-31 17:13:22 -0200

    Adding runner for production

---
+++ scripts/run.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import sys
+sys.path.append('/usr/lib/mailman')
+sys.path.append('..')
+
+
+import bottle
+from paste import httpserver
+from mailmanapi import *
+from mailmanapi.settings import ALLOWED_IPS
+
+
+bottle_app = bottle.default_app()
+def application(environ, start_response):
+    if environ['REMOTE_ADDR'] not in ALLOWED_IPS:
+        status = '403 FORBIDDEN'
+        headers = [('Content-type', 'text/plain')]
+        start_response(status, headers)
+        return 'FORBIDDEN'
+
+    return bottle_app(environ, start_response)
+
+httpserver.serve(application, host='0.0.0.0', port=8000)
