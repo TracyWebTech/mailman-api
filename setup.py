a1371e5f8f9e06e3c95b977c4f9b9acddb1b9207
Sergio Oliveira <sergio@tracy.com.br>
2014-09-17 18:47:40 -0300

    Version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='mailman-api',
-    version='0.1.0',
+    version='0.2.0',
     author='Sergio Oliveira',
     author_email='sergio@tracy.com.br',
     packages=['mailmanapi'],
