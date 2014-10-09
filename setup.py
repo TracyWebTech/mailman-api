44e4dae2d6bc4c3cd3b3dab41415fefab619207e
Antonio Terceiro <terceiro@softwarelivre.org>
2014-10-09 15:28:47 -0300

    Final touches in upstream packaging

--- setup.py
+++ setup.py
@@ -3,16 +3,18 @@ from setuptools import setup
 
 setup(
     name='mailman-api',
-    version='0.2.2',
+    version='0.2.3',
     author='Sergio Oliveira',
     author_email='sergio@tracy.com.br',
     packages=['mailmanapi'],
     package_data={'mailmanapi': ['templates/*']},
-    scripts=['scripts/mailman-api.py'],
+    scripts=['scripts/mailman-api'],
     url='http://pypi.python.org/pypi/mailman-api/',
     license='LICENSE.txt',
-    description='RESTful API to access some funcionalities of Mailman 2',
-    long_description=open('README.rst').read(),
+    description='REST API daemon to interact with Mailman 2',
+    long_description="""mailman-api provides a daemon that will listen to HTTP requests, providing
+access to a REST API that can be used to interact with a locally-installed
+Mailman instance.""",
     install_requires=[
         "Paste >= 1.7.5.1",
         "bottle >= 0.11.6",
