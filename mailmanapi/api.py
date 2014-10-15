f1e14a91f9a5d2972681753ab6d0b8b963b15b2d
Sergio Oliveira <sergio@tracy.com.br>
2014-10-15 10:49:48 -0300

    Added docstrings on rest methods

--- mailmanapi/api.py
+++ mailmanapi/api.py
@@ -15,6 +15,14 @@ EMAIL_TEMPLATE = os.path.join(CWD, 'templates', 'message.tpl')
 
 @route('/', method='GET')
 def list_lists():
+    """Lists existing mailing lists on the server.
+
+    **Method**: GET
+
+    **URI**: /
+
+    Returns a list of the mailing lists that exist on this server."""
+
     all_lists = Utils.list_names()
     lists = []
 
@@ -35,6 +43,21 @@ def list_lists():
 
 @route('/<listname>', method='PUT')
 def subscribe(listname):
+    """Adds a new subscriber to the list called `<listname>`
+
+    **Method**: PUT
+
+    **URI**: /<listname>
+
+    **Parameters**:
+
+      * `address`: email address that is to be subscribed to the list.
+      * `fullname`: full name of the person being subscribed to the list.
+      * `digest`: if this equals `true`, the new subscriber will receive
+        digests instead of every mail sent to the list.
+
+    """
+
     address = request.forms.get('address')
     fullname = request.forms.get('fullname')
     digest = parse_boolean(request.forms.get('digest'))
@@ -61,6 +84,18 @@ def subscribe(listname):
 
 @route('/<listname>', method='DELETE')
 def unsubscribe(listname):
+    """Unsubsribe an email address from the mailing list.
+
+    **Method**: DELETE
+
+    **URI**: /<listname>
+
+    **Parameters**:
+
+      * `address`: email address that is to be unsubscribed from the list
+
+    """
+
     address = request.forms.get('address')
     mlist = get_mailinglist(listname)
 
@@ -77,12 +112,35 @@ def unsubscribe(listname):
 
 @route('/<listname>', method='GET')
 def members(listname):
+    """Lists subscribers for the `listname` list.
+
+    **Method**: GET
+
+    **URI**: /<listname>
+
+    Returns an array of email addresses."""
+
     mlist = get_mailinglist(listname, lock=False)
     return jsonify(mlist.getMembers())
 
 
 @route('/<listname>/sendmail', method='POST')
 def sendmail(listname):
+    """Posts an email to the mailing list.
+
+    **Method**: POST
+
+    **URI**: /<listname>/sendmail
+
+    **Parameters**:
+
+      * `name_from`: name of the poster
+      * `email_from`: email address of the poster
+      * `subject`: the subject of the message
+      * `body`: the body of the message.
+      * `in_reply_to` (optional): Message-ID of the message that is being
+        replied to, if any."""
+
     mlist = get_mailinglist(listname, lock=False)
 
     context = {}
@@ -111,6 +169,7 @@ def sendmail(listname):
 
 def get_application(allowed_ips):
     bottle_app = default_app()
+
     def application(environ, start_response):
         if environ['REMOTE_ADDR'] not in allowed_ips:
             status = '403 FORBIDDEN'
