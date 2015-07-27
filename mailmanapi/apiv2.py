8e8e9fa9722b0c8de2c3fbbd412441922b2db7fe
Charles Oliveira <18oliveira.charles@gmail.com>
2015-07-27 15:36:12 -0400

    Added version 2 of the mailman api, with user-defined error code, instead of HTTP ones

--- mailmanapi/apiv2.py
+++ mailmanapi/apiv2.py
@@ -1,4 +1,3 @@
-
 import os
 import uuid
 import logging
@@ -10,52 +9,26 @@ try:
 except ImportError:
     logging.error('Could not import Mailman module')
 
-from .members import Member
 from .utils import parse_boolean, jsonify, get_mailinglist, get_timestamp
 
 
 CWD = os.path.abspath(os.path.dirname(__file__))
 EMAIL_TEMPLATE = os.path.join(CWD, 'templates', 'message.tpl')
-API_VERSION = '/v2'
+
+ERRORS_CODE = {
+    'Ok': 0,
+    'MMSubscribeNeedsConfirmation': 1,
+    'MMNeedApproval': 2,
+    'MMAlreadyAMember': 3,
+    'MembershipIsBanned': 4,
+    'MMBadEmailError': 5,
+    'MMHostileAddress': 6,
+    'NotAMemberError': 7,
+    'MissingInformation': 8,
+}
 
 
-@route(API_VERSION + '/', method='GET')
-def list_listsv2():
-    """Lists existing mailing lists on the server.
-
-    **Method**: GET
-
-    **URI**: /
-
-    Returns a list of the mailing lists that exist on this server."""
-
-    all_lists = Utils.list_names()
-    lists = []
-
-    include_description = request.query.get('description')
-    include_private = request.query.get('private')
-
-    address = request.query.get('address')
-    for listname in all_lists:
-        mlist = get_mailinglist(listname, lock=False)
-        members = mlist.getMembers()
-        if not address or address in members:
-            list_values = [listname]
-            if include_description:
-                list_values.append(mlist.description.decode('latin1'))
-            if include_private:
-                list_values.append(bool(mlist.archive_private))
-
-            if len(list_values) == 1:
-                lists.append(list_values[0])
-            else:
-                lists.append(list_values)
-
-    return jsonify(lists)
-
-
-@route(API_VERSION + '/<listname>', method='PUT')
-def subscribev2(listname):
+def subscribe(listname):
     """Adds a new subscriber to the list called `<listname>`
 
     **Method**: PUT
@@ -77,20 +50,17 @@ def subscribev2(listname):
 
     mlist = get_mailinglist(listname)
     userdesc = UserDesc.UserDesc(address, fullname, digest=digest)
-    result = jsonify('Subscription succeeded!')
+    result = jsonify(ERRORS_CODE['Ok'])
 
     try:
         mlist.AddMember(userdesc)
-    except Errors.MMSubscribeNeedsConfirmation:
-        result = jsonify("A confirmation was sent to you, please check your email.")
-    except Errors.MMNeedApproval:
-        result = jsonify("Your subscription was sent successfully! Please wait for the list's admin approval.")
-    except Errors.MMAlreadyAMember:
-        result = jsonify("You are already a member of this list.")
-    except Errors.MembershipIsBanned:
-        result = jsonify("You are banned from this list!")
-    except (Errors.MMBadEmailError, Errors.MMHostileAddress):
-        result = jsonify("Invalid address.")
+    except (Errors.MMSubscribeNeedsConfirmation,
+            Errors.MMNeedApproval,
+            Errors.MMAlreadyAMember,
+            Errors.MembershipIsBanned,
+            Errors.MMBadEmailError,
+            Errors.MMHostileAddress), e:
+        result = jsonify(ERRORS_CODE[e.__class__.__name__])
     finally:
         mlist.Save()
         mlist.Unlock()
@@ -98,8 +68,7 @@ def subscribev2(listname):
     return result
 
 
-@route(API_VERSION + '/<listname>', method='DELETE')
-def unsubscribev2(listname):
+def unsubscribe(listname):
     """Unsubsribe an email address from the mailing list.
 
     **Method**: DELETE
@@ -114,35 +83,20 @@ def unsubscribev2(listname):
 
     address = request.forms.get('address')
     mlist = get_mailinglist(listname)
-    result = jsonify('Unsubscription succeeded!')
+    result = jsonify(ERRORS_CODE['Ok'])
 
     try:
         mlist.ApprovedDeleteMember(address, admin_notif=False, userack=True)
-        mlist.Save()
-    except Errors.NotAMemberError:
-        result = jsonify("Not a member.")
+    except Errors.NotAMemberError, e:
+        result = jsonify(ERRORS_CODE[e.__class__.__name__])
     finally:
+        mlist.Save()
         mlist.Unlock()
 
     return result
 
 
-@route(API_VERSION + '/<listname>', method='GET')
-def membersv2(listname):
-    """Lists subscribers for the `listname` list.
-
-    **Method**: GET
-
-    **URI**: /<listname>
-
-    Returns an array of email addresses."""
-
-    mlist = get_mailinglist(listname, lock=False)
-    return jsonify(mlist.getMembers())
-
-
-@route(API_VERSION + '/<listname>/sendmail', method='POST')
-def sendmailv2(listname):
+def sendmail(listname):
     """Posts an email to the mailing list.
 
     **Method**: POST
@@ -175,24 +129,11 @@ def sendmailv2(listname):
     if in_reply_to:
         context['in_reply_to'] = in_reply_to
 
+    result = jsonify(ERRORS_CODE['Ok'])
+
     if None in context.values():
-        return jsonify('Missing information. `email_from`, `subject` and '
-                       '`body` are mandatory', 400)
+        result = jsonify(ERRORS_CODE['MissingInformation'])
 
     email = template(EMAIL_TEMPLATE, context)
     Post.inject(listname, email.encode('utf8'), qdir=mm_cfg.INQUEUE_DIR)
-    return jsonify(True)
-
-
-def get_application(allowed_ips):
-    bottle_app = default_app()
-
-    def application(environ, start_response):
-        if environ['REMOTE_ADDR'] not in allowed_ips:
-            status = '403 FORBIDDEN'
-            headers = [('Content-type', 'text/plain')]
-            start_response(status, headers)
-            return 'FORBIDDEN'
-
-        return bottle_app(environ, start_response)
-    return application
+    return result
