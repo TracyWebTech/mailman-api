9fe490550ad1921a5e66a1240d105e523998cb01
Sergio Oliveira <sergio@tracy.com.br>
2015-07-31 17:36:14 -0300

    Fixed bug sending data to coveralls

--- tests/run.sh
+++ tests/run.sh
@@ -6,6 +6,6 @@ DOCKER_IMAGE="tracywebtech/mailman-api"
 
 DOCKER_CMD="docker run -w /srv/mailman-api -v $DIR/../:/srv/mailman-api $DOCKER_IMAGE"
 
-$DOCKER_CMD python setup.py nosetests --with-coverage --cover-package=mailmanapi
+$DOCKER_CMD python setup.py nosetests --with-coverage --cover-package=mailmanapi --cover-erase
 
 $DOCKER_CMD flake8 mailmanapi
