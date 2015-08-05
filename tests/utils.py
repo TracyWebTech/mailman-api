f6f1387705877f560776503890037827b5147a9b
Charles Oliveira <18oliveira.charles@gmail.com>
2015-08-05 14:10:24 -0400

    Added create list method

--- tests/utils.py
+++ tests/utils.py
@@ -1,9 +1,9 @@
 
 import unittest
+import os
 from webtest import TestApp
-
-
 from mailmanapi.routes import get_application
+from Mailman import MailList, UserDesc
 
 
 class MailmanAPITestCase(unittest.TestCase):
@@ -14,3 +14,11 @@ class MailmanAPITestCase(unittest.TestCase):
         self.client = TestApp(application, extra_environ={
             'REMOTE_ADDR': '127.0.0.1',
         })
+
+    def create_list(self, list_name, list_admin = 'admin@list.com', 
+                    list_pass = '123456', subscribe_policy = 0):
+        m = MailList.MailList()
+        m.Create(list_name, list_admin, list_pass)
+        m.subscribe_policy = subscribe_policy
+        m.Save()
+        m.Unlock()
