b1d4a4dc286a4c128fddcaac36755334ed3504af
msfernandes <matheus.souza.fernandes@gmail.com>
2015-08-06 10:14:42 -0300

    Update api v2 routes
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>
    Signed-off-by: Charles Oliveira <18oliveira.charles@gmail.com>

--- mailmanapi/routes.py
+++ mailmanapi/routes.py
@@ -7,19 +7,30 @@ from . import apiv2
 def create_routes(app):
 
     # v1
-    app.route('/', method='GET', callback=apiv1.list_lists)
-    app.route('/<listname>', method='PUT', callback=apiv1.subscribe)
-    app.route('/<listname>', method='DELETE', callback=apiv1.unsubscribe)
-    app.route('/<listname>', method='GET', callback=apiv1.members)
-    app.route('/<listname>/sendmail', method='POST', callback=apiv1.sendmail)
+    app.route('/', method='GET',
+              callback=apiv1.list_lists)
+    app.route('/<listname>', method='PUT',
+              callback=apiv1.subscribe)
+    app.route('/<listname>', method='DELETE',
+              callback=apiv1.unsubscribe)
+    app.route('/<listname>', method='GET',
+              callback=apiv1.members)
+    app.route('/<listname>/sendmail', method='POST',
+              callback=apiv1.sendmail)
 
     # v2
-    # app.route('/v2/', method='GET', callback=apiv1.list_lists)
-    app.route('/v2/<listname>', method='PUT', callback=apiv2.subscribe)
-    app.route('/v2/<listname>', method='DELETE', callback=apiv2.unsubscribe)
-    # app.route('/v2/<listname>', method='GET', callback=apiv1.members)
-    # app.route('/v2/<listname>/sendmail', method='POST',
-    #           callback=apiv1.sendmail)
+    app.route('/v2/lists/', method='GET',
+              callback=apiv1.list_lists)
+    app.route('/v2/lists/<listname>', method='POST',
+              callback=apiv2.create_list)
+    app.route('/v2/subscribe/<listname>', method='PUT',
+              callback=apiv2.subscribe)
+    app.route('/v2/subscribe/<listname>', method='DELETE',
+              callback=apiv2.unsubscribe)
+    app.route('/v2/members/<listname>', method='GET',
+              callback=apiv1.members)
+    app.route('/v2/sendmail/<listname>', method='POST',
+              callback=apiv1.sendmail)
 
 
 def get_application(allowed_ips):
