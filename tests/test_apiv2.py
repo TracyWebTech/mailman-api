99c0f7407279877674036599051455d0b1c3ca65
Matheus Fernandes <matheus.souza.fernandes@gmail.com>
2015-08-06 17:07:32 -0300

    Finished apiv2 coverage
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>
    Signed-off-by: Charles Oliveira <18oliveira.charles@gmail.com>

--- tests/test_apiv2.py
+++ tests/test_apiv2.py
@@ -117,6 +117,25 @@ class TestAPIv2(MailmanAPITestCase):
         self.assertEqual(resp.status_code, 200)
         self.assertEqual(0, resp.json)
 
+    def test_sendmail_with_reply(self):
+        path = 'sendmail/'
+        mlist = MailList.MailList(self.list_name)
+        data = {}
+        data['email_to'] = mlist.GetListEmail()
+        data['message_id'] = 1
+        data['ip_from'] = '127.0.0.1'
+        data['timestamp'] = strftime('%a, %d %b %Y %H:%M:%S %z (%Z)')
+        data['name_from'] = 'user test'
+        data['email_from'] = self.data['address']
+        data['subject'] = 'subject test'
+        data['body'] = 'body test'
+        data['in_reply_to'] = 1
+
+        resp = self.client.post(self.url + path + self.list_name,
+                                data, expect_errors=True)
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(0, resp.json)
+
     def test_sendmail_missing_information(self):
         path = 'sendmail/'
         data = {}
@@ -171,12 +190,159 @@ class TestAPIv2(MailmanAPITestCase):
         new_list = 'new_list'
         url = self.url + 'lists/' + new_list
 
-        data = {'admin': self.data['address'], 'password': '123456',
-                'subscription_policy': 1, 'archive_privacy': 1}
+        data = {'admin': self.data['address'], 'password': '123456'}
 
         resp = self.client.post(url, data, expect_errors=True)
         self.assertEqual(resp.status_code, 200)
         self.assertEqual(0, resp.json)
+        self.remove_list(new_list)
+
+    def test_create_list_already_exists(self):
+        new_list = self.list_name
+        url = self.url + 'lists/' + new_list
+
+        data = {'admin': self.data['address'], 'password': '123456'}
+
+        resp = self.client.post(url, data, expect_errors=True)
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(13, resp.json)
+
+    def test_create_private_list(self):
+        new_list = "new_list"
+        url = self.url + 'lists/' + new_list
+
+        data = {'admin': self.data['address'], 'password': '123456',
+                'archive_private': 1}
+        resp = self.client.post(url, data, expect_errors=True)
+        mlist = MailList.MailList(new_list)
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(int(mlist.archive_private), 1)
+        mlist.Unlock()
+        self.remove_list(new_list)
+
+    def test_create_public_list(self):
+        new_list = "new_list"
+        url = self.url + 'lists/' + new_list
+
+        data = {'admin': self.data['address'], 'password': '123456',
+                'archive_private': 0}
+        resp = self.client.post(url, data, expect_errors=True)
+        mlist = MailList.MailList(new_list)
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(int(mlist.archive_private), 0)
+        mlist.Unlock()
+        self.remove_list(new_list)
+
+    def test_create_list_archive_private_out_of_max_range(self):
+        new_list = "new_list"
+        url = self.url + 'lists/' + new_list
+
+        data = {'admin': self.data['address'], 'password': '123456',
+                'archive_private': 2}
+        resp = self.client.post(url, data, expect_errors=True)
+        mlist = MailList.MailList(new_list)
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(int(mlist.archive_private), 0)
+        mlist.Unlock()
+        self.remove_list(new_list)
+
+    def test_create_list_archive_private_out_of_min_range(self):
+        new_list = "new_list"
+        url = self.url + 'lists/' + new_list
+
+        data = {'admin': self.data['address'], 'password': '123456',
+                'archive_private': -1}
+        resp = self.client.post(url, data, expect_errors=True)
+        mlist = MailList.MailList(new_list)
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(int(mlist.archive_private), 0)
+        mlist.Unlock()
+        self.remove_list(new_list)
+
+    def test_create_confirm_list(self):
+        new_list = "new_list"
+        url = self.url + 'lists/' + new_list
+
+        data = {'admin': self.data['address'], 'password': '123456',
+                'subscribe_policy': 1}
+        resp = self.client.post(url, data, expect_errors=True)
+        mlist = MailList.MailList(new_list)
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(int(mlist.subscribe_policy), 1)
+        mlist.Unlock()
+        self.remove_list(new_list)
+
+    def test_create_approval_list(self):
+        new_list = "new_list"
+        url = self.url + 'lists/' + new_list
+
+        data = {'admin': self.data['address'], 'password': '123456',
+                'subscribe_policy': 2}
+        resp = self.client.post(url, data, expect_errors=True)
+        mlist = MailList.MailList(new_list)
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(int(mlist.subscribe_policy), 2)
+        mlist.Unlock()
+        self.remove_list(new_list)
+
+    def test_create_confirm_and_approval_list(self):
+        new_list = "new_list"
+        url = self.url + 'lists/' + new_list
+
+        data = {'admin': self.data['address'], 'password': '123456',
+                'subscribe_policy': 3}
+        resp = self.client.post(url, data, expect_errors=True)
+        mlist = MailList.MailList(new_list)
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(int(mlist.subscribe_policy), 3)
+        mlist.Unlock()
+        self.remove_list(new_list)
+
+    def test_create_list_subscribe_policy_out_of_max_range(self):
+        new_list = "new_list"
+        url = self.url + 'lists/' + new_list
+
+        data = {'admin': self.data['address'], 'password': '123456',
+                'subscribe_policy': 4}
+        resp = self.client.post(url, data, expect_errors=True)
+        mlist = MailList.MailList(new_list)
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(int(mlist.subscribe_policy), 1)
+        mlist.Unlock()
+        self.remove_list(new_list)
+
+    def test_create_list_subscribe_policy_out_of_min_range(self):
+        new_list = "new_list"
+        url = self.url + 'lists/' + new_list
+
+        data = {'admin': self.data['address'], 'password': '123456',
+                'subscribe_policy': 0}
+        resp = self.client.post(url, data, expect_errors=True)
+        mlist = MailList.MailList(new_list)
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(int(mlist.subscribe_policy), 1)
+        mlist.Unlock()
+        self.remove_list(new_list)
+
+    def test_create_list_invalid_params(self):
+        new_list = "new_list"
+        url = self.url + 'lists/' + new_list
+
+        data = {'admin': self.data['address'], 'password': '123456',
+                'subscribe_policy': 'Invalid', 'archive_private': 'Invalid'}
+        resp = self.client.post(url, data, expect_errors=True)
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(resp.json, 14)
+        self.remove_list(new_list)
+
+    def test_create_list_invalid_password(self):
+        new_list = "new_list"
+        url = self.url + 'lists/' + new_list
+
+        data = {'admin': self.data['address'], 'password': ''}
+        resp = self.client.post(url, data, expect_errors=True)
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(resp.json, 11)
         self.remove_list(new_list)
 
     def test_members(self):
